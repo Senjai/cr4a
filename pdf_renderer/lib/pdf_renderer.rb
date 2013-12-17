31db277f8574bb093e4cf9e1987c9ce71a44bad3
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2013-12-16 22:51:29 -0800

    Render pdfs
    Change-Id: I1811a09d068780017f05da5dd170bd56bb2134bf

--- pdf_renderer/lib/pdf_renderer.rb
+++ pdf_renderer/lib/pdf_renderer.rb
@@ -1,2 +1,7 @@
-module PdfRenderer
+require 'prawn'
+
+ActionController::Renderers.add :pdf do |filename, options|
+  pdf = Prawn::Document.new
+  pdf.text render_to_string(options)
+  send_data(pdf.render, filename: "#{filename}.pdf", disposition: "attachment")
 end
