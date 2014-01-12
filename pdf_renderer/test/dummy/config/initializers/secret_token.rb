9a95bd3c09a440c8787d87f9dfdc06d686b8fed9
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2014-01-11 16:16:26 -0800

    Basic draft
    Change-Id: I32e08a433e7bbf68484a88df39cc9fa54da16b69

---
+++ pdf_renderer/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'ac374c398a582159c5be809bb532a3d000b4105bfa6088887c3fdf324ec5756552767637531a9bb060141bc4988262593587e072802a1f891ee8927795e83428'
