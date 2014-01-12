440c34d1148ce8c03ca0248474a6c3fbcf5412d4
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2014-01-11 16:23:22 -0800

    Make sure the renderer uses the specified template
    Change-Id: If07bb927e9f39631ce17fba3291becdf6a016620

--- pdf_renderer/test/integration/pdf_delivery_test.rb
+++ pdf_renderer/test/integration/pdf_delivery_test.rb
@@ -6,7 +6,15 @@ class PdfDeliveryTest < ActionDispatch::IntegrationTest
 
     assert_match "PDF", response.body
     assert_equal "binary", headers["Content-Transfer-Encoding"]
+    assert_equal "attachment; filename=\"contents.pdf\"", headers["Content-Disposition"]
+    assert_equal "application/pdf", headers["Content-Type"]
+  end
 
+  test "pdf renderer uses the specified template" do
+    get another_path(format: :pdf)
+
+    assert_match "PDF", response.body
+    assert_equal "binary", headers["Content-Transfer-Encoding"]
     assert_equal "attachment; filename=\"contents.pdf\"", headers["Content-Disposition"]
     assert_equal "application/pdf", headers["Content-Type"]
   end
