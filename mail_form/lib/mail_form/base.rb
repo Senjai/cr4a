f176d9ff323b149b20b2085e35a8791d2146840e
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2014-01-11 17:07:41 -0800

    Add ? suffix
    Change-Id: I6975a7c7e50fda09697afeb652ce2618047f0251

--- mail_form/lib/mail_form/base.rb
+++ mail_form/lib/mail_form/base.rb
@@ -1,7 +1,23 @@
 module MailForm
   class Base
+    include ActiveModel::AttributeMethods
+
+    attribute_method_prefix 'clear_'
+    attribute_method_suffix '?'
+
     def self.attributes(*names)
       attr_accessor(*names)
+      define_attribute_methods(names)
+    end
+
+    protected
+
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)
+    end
+
+    def attribute?(attribute)
+      send(attribute).present?
     end
   end
 end
--- mail_form/test/mail_form_test.rb
+++ mail_form/test/mail_form_test.rb
@@ -8,5 +8,21 @@ class MailFormTest < ActiveSupport::TestCase
     assert_equal "User", sample.name
     sample.email = "test@test.com"
     assert_equal "test@test.com", sample.email
+
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.clear_name
+    assert_nil sample.clear_email
+  end
+
+  test "sample mail can ask if an attribute is present or not" do
+    sample = SampleMail.new
+    assert !sample.name?
+
+    sample.name = "User"
+    assert sample.name?
+
+    sample.email = ""
+    assert !sample.email?
   end
 end
