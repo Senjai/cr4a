b709a5f3aaa78a713dad49535c264ef773949174
Richard Wilson <richard@freerunningtech.com>
2014-02-01 14:26:38 -0800

    Impliment resolver functionality
    Change-Id: Ie9d9a87f789f0267c9257c8100c25a4eae048070

--- templater/test/models/sql_template_test.rb
+++ templater/test/models/sql_template_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class SqlTemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "resolver returns a template with the saved body" do
+    resolver = SqlTemplate::Resolver.new
+    details = { formats: [:html], locale: [:en], handlers: [:erb] }
+
+    # Assert resolver cannot find any template as the database is empty
+    assert resolver.find_all("index", "posts", false, details).empty?
+
+    SqlTemplate.create!(
+      body: "<%= 'Hi from SqlTemplate!' %>",
+      path: "posts/index",
+      format: "html",
+      locale: "en",
+      handler: "erb",
+      partial: false
+    )
+
+    template = resolver.find_all("index", "posts", false, details).first
+    assert_kind_of ActionView::Template, template
+
+    assert_equal "<%= 'Hi from SqlTemplate!' %>", template.source
+    assert_kind_of ActionView::Template::Handlers::ERB, template.handler
+    assert_equal [:html], template.formats
+    assert_equal "posts/index", template.virtual_path
+    assert_match %r[SqlTemplate - \d+ - "posts/index"], template.identifier
+  end
 end
