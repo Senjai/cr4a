1ab5fba73719455c52f93b87e866e5b18122a951
Richard Wilson <richard@freerunningtech.com>
2014-02-01 14:43:08 -0800

    Use SqlTemplate to render 'users/index' instead of the filesystem
    Change-Id: I8bd9b81d7fa88b87260d4da27dffd5dcb45fbc8b

--- templater/app/controllers/users_controller.rb
+++ templater/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  append_view_path SqlTemplate::Resolver.new
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
--- templater/db/schema.rb
+++ templater/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140201214416) do
+ActiveRecord::Schema.define(version: 20140201223236) do
 
   create_table "sql_templates", force: true do |t|
     t.text     "body"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140201214416) do
     t.string   "locale"
     t.string   "handler"
     t.boolean  "partial",    default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
