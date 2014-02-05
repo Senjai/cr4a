8d407968a5247f7c5a72f5f9384622c2fe71a574
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2014-02-04 18:33:43 -0800

    rails plugin new handler
    Change-Id: I222244727eca3c0efcc71033794a97915ba535d1

---
+++ handlers/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '8a856f2947bc2467a4c462e39696f801d8ce0035968c0c3dfd0f766ed146abc7edf4e274dd00a4f9cc5be32a465f3cf296c01e88db22097ee6da21190331ef38'
